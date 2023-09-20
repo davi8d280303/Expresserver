fe6cd548a13e9a65f1076ccdc6fdc11d3edbad85
David <davidaguilarodriguez28@gmail.com>
2023-09-19 22:38:17 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+app.get("/", (req, res) => {
+   //res.send("hola mundo!");
+   res.sendFile(path.join(__dirname + "/index.html"));
+});
+
+app.listen(8000, () => {
+    console.log("server listening on port", 8000); 
+});
